39ef3c16f38738672aae9691f47c7580bbcf1e29
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-02-26 16:46:54 +0530

    landing page started

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -14,7 +14,7 @@ const Features = () => {
   };
 
   return (
-    <div className="bg-[url('/static/images/features_bg.avif')] bg-cover bg-right-top">
+    <div className="features">
       <div className="m-3 md:ml-10">
         <div>
           <h1 className="p-2 text-center md:text-left text-6xl">Features</h1>
@@ -22,10 +22,10 @@ const Features = () => {
             Uncover a seamless and intuitive user experience as you navigate through advanced tools and functionalities designed to enhance your interaction and engagement. Discover a world of convenience and innovation right at your fingertips.
           </p>
         </div>
-        <div className="md:grid grid-cols-2 mt-10">
+        <div className="md:grid grid-cols-2 mt-10 gap-10">
           <div className="flex flex-col">
             {feat_det.map((ele, index) => (
-              <div key={index} className="p-3 md:p-5 border-2 border-white text-white bg-gradient-to-b from-gray-600 to-black">
+              <div key={index} className="p-3 md:p-5 border-2 border-black text-black bg-white">
                 <p className="flex text-2xl items-center">
                   {<ele.icon className="mr-1" size={40} />}
                   {ele.title}
@@ -42,7 +42,7 @@ const Features = () => {
             ))}
           </div>
           <div>
-            <img className="w-1/2 m-auto rounded-2xl" src="/static/images/Laptop.jpeg" alt="" />
+            <img className="w-[80%] m-auto shadow-2xl" src="/static/images/Laptop.jpeg" alt="" />
           </div>
         </div>
       </div>
