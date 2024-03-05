2c5ef22b28653d99caf80ba8fc342c3801ff8388
sumitt-kun <sumitkumar933434@gmail.com>
2024-03-06 01:29:19 +0530

    added login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,39 +4,38 @@ function Navbar() {
   return (
     <div className="flex z-20 my-2">
       <div className="p-2 ml-10 text-3xl flex">
-        <p className="m-2 py-2 invisible md:visible flex"><img className="px-2" src="/static/images/favicon-32x32.png" alt="" />PEARL CREST</p>
+        <p className="m-2 py-2 invisible md:visible flex">
+          <img className="px-2" src="/static/images/favicon-32x32.png" alt="" />
+          PEARL CREST
+        </p>
       </div>
       <div className="ml-auto">
         <ul className="flex">
           <li className="p-3 text-lg mx-5 group">
             <button className="relative overflow-hidden py-2 px-4">
-            Home
-            <span className="absolute inset-x-0 bottom-0 w-1/2 h-1 group-hover:w-full bg-black"></span>
+              Home
+              <span className="absolute inset-x-0 bottom-0 w-1/2 h-1 group-hover:w-full bg-black"></span>
             </button>
           </li>
           <li className="p-3 text-lg mx-5 group">
             <button className="relative overflow-hidden py-2 px-4">
-            Gallery
-            <span className="absolute hover:visible inset-x-0 bottom-0 w-1/2 group-hover:w-full h-1 bg-black"></span>
+              Gallery
+              <span className="absolute hover:visible inset-x-0 bottom-0 w-1/2 group-hover:w-full h-1 bg-black"></span>
             </button>
           </li>
           <li className="p-3 text-lg mx-5 group">
             <button className="relative overflow-hidden py-2 px-4">
-            Council
-            <span className="absolute hover:visible inset-x-0 bottom-0 w-1/2 group-hover:w-full h-1 bg-black"></span>
+              Council
+              <span className="absolute hover:visible inset-x-0 bottom-0 w-1/2 group-hover:w-full h-1 bg-black"></span>
             </button>
           </li>
           <li className="p-3 text-lg px-10 mx-5 hover:bg-black hover:text-white border-black border-2 m-2 rounded-l-3xl rounded-r-3xl">
-            <Link to="/db">
-            <button>
-                Login
-            </button>
+            <Link to="/log">
+              <button>Login</button>
             </Link>
           </li>
           <li className="p-3 text-lg hover:opacity-90 text-white bg-black px-10 mx-5 mr-8 m-2 border-black border-2 rounded-l-3xl rounded-r-3xl">
-            <button>
-                Sign Up
-            </button>
+            <button>Sign Up</button>
           </li>
         </ul>
       </div>
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,43 +1,82 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-function Showcase(){
-    const [isVisible, setIsVisible] = useState(false);
+function Showcase() {
+  const [isVisible, setIsVisible] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollY = window.scrollY;
-            const threshold = -1; // Adjust this value based on your needs
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const threshold = -1; // Adjust this value based on your needs
 
-            setIsVisible(scrollY > threshold);
-        };
+      setIsVisible(scrollY > threshold);
+    };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-    return(
-        <div className="m-0 md:h-[30rem]">
-        <div className="flex flex-col-reverse md:flex-row items-center p-6 mx-auto space-y-0">
-            <div className="flex flex-col ml-5 md:mb-32 space-y-12 md:w-2/3">
-                <h2 className="w-full m-0 md:p-1 md:mt-10 text-3xl text-center md:text-5xl md:text-left text-black ">From Payments to Complaints: Pearl Crest Puts it All at Your Fingertips</h2>
-                <button className="p-3 text-center bg-black hover:opacity-80 w-1/4 m-auto md:m-0 border-white text-white border-2 rounded-l-3xl rounded-r-3xl">
-                Login
-                </button>
-            </div>
-            <div className="w-1/2 h-full invisible md:visible">
-                <img className={`${isVisible ? "show":""} showcase_img1 overflow-x-hidden w-1/3 border-2 border-black  absolute top-[60%] left-[63%] rounded-3xl rotate-12`} src="/static/images/PC2.jpg" alt="" />\
-                <img className={`${isVisible ? "show":""} showcase_img2 w-1/3 border-2 border-black  absolute top-[20%] left-[52%] rounded-3xl rotate-12`} src="/static/images/PC4.jpg" alt="" />\
-                <img className={`${isVisible ? "show":""} showcase_img3 w-1/3 border-2 border-black absolute top-1/6 left-[32%] rounded-3xl rotate-12`} src="/static/images/PC5.jpg" alt="" />\
-            </div>
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div className="m-0 md:h-[30rem]">
+      <div className="flex flex-col-reverse md:flex-row items-center p-6 mx-auto space-y-0">
+        <div className="flex flex-col ml-5 md:mb-32 space-y-12 md:w-2/3">
+          <h2 className="w-full m-0 md:p-1 md:mt-10 text-3xl text-center md:text-5xl md:text-left text-black ">
+            From Payments to Complaints: Pearl Crest Puts it All at Your
+            Fingertips
+          </h2>
+          <Link to="/log">
+            <button className="p-3 text-center bg-black hover:opacity-80 w-1/4 m-auto md:m-0 border-white text-white border-2 rounded-l-3xl rounded-r-3xl">
+              Login
+            </button>
+          </Link>
         </div>
-        <div className="md:hidden h-full m-1 grid gap-4 overflow-hidden">
-                {/* <img className={`w-full m-auto border-2 border-black rounded-3xl`} src="../../public/static/images/PC2.jpg" alt="" /> */}
-                <img className={`showcase_img1 ${isVisible ? "show":""} w-1/2 m-auto border-2 border-black rounded-3xl`} src="../../public/static/images/PC4.jpg" alt="" />
-                <img className={`showcase_img3 ${isVisible ? "show":""} w-1/2 m-auto border-2 border-black rounded-3xl`} src="../../public/static/images/PC5.jpg" alt="" />
+        <div className="w-1/2 h-full invisible md:visible">
+          <img
+            className={`${
+              isVisible ? "show" : ""
+            } showcase_img1 overflow-x-hidden w-1/3 border-2 border-black  absolute top-[60%] left-[63%] rounded-3xl rotate-12`}
+            src="/static/images/PC2.jpg"
+            alt=""
+          />
+          \
+          <img
+            className={`${
+              isVisible ? "show" : ""
+            } showcase_img2 w-1/3 border-2 border-black  absolute top-[20%] left-[52%] rounded-3xl rotate-12`}
+            src="/static/images/PC4.jpg"
+            alt=""
+          />
+          \
+          <img
+            className={`${
+              isVisible ? "show" : ""
+            } showcase_img3 w-1/3 border-2 border-black absolute top-1/6 left-[32%] rounded-3xl rotate-12`}
+            src="/static/images/PC5.jpg"
+            alt=""
+          />
+          \
         </div>
-        </div>
-    )
+      </div>
+      <div className="md:hidden h-full m-1 grid gap-4 overflow-hidden">
+        {/* <img className={`w-full m-auto border-2 border-black rounded-3xl`} src="../../public/static/images/PC2.jpg" alt="" /> */}
+        <img
+          className={`showcase_img1 ${
+            isVisible ? "show" : ""
+          } w-1/2 m-auto border-2 border-black rounded-3xl`}
+          src="../../public/static/images/PC4.jpg"
+          alt=""
+        />
+        <img
+          className={`showcase_img3 ${
+            isVisible ? "show" : ""
+          } w-1/2 m-auto border-2 border-black rounded-3xl`}
+          src="../../public/static/images/PC5.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Showcase;
